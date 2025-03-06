17d75daa88bc44796a309ac498b91ab6e9bc4760
michalbukalski <michalbukalski@gmail.com>
2025-03-06 01:08:57 +0100

    add some challange

--- src/ChallangeReadingUserInput.java
+++ src/ChallangeReadingUserInput.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class ChallangeReadingUserInput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 1;
+        int sum = 0;
+
+        while (counter<=5){
+            System.out.println("Enter number #"+counter+":");
+            String nextNumber = scanner.next();
+            try {
+                int number = Integer.parseInt(nextNumber);
+                counter++;
+                sum+=number;
+            } catch (NumberFormatException nfe){
+                System.out.println("Invalid number");
+            }
+        }
+
+        System.out.println("The sum of entered number is: "+sum);
+    }
 }
--- src/ChallangeReadingUserInput_DECIMAL.java
+++ src/ChallangeReadingUserInput_DECIMAL.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ChallangeReadingUserInput_DECIMAL {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 1;
+        double sum = 0;
+
+        while (counter<=5){
+            System.out.println("Enter number #"+counter+":");
+            String nextNumber = scanner.next();
+            try {
+//                int number = Integer.parseInt(nextNumber);
+                double number = Double.parseDouble(nextNumber);
+                counter++;
+                sum+=number;
+            } catch (NumberFormatException nfe){
+                System.out.println("Invalid number");
+            }
+        }
+
+        System.out.println("The sum of entered number is: "+sum);
+    }
 }
